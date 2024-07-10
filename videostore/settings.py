f91856cd43c9e64e0ce2669696a0bd2b7bc1bf59
d-rubin <daniel.rubin1997@gmail.com>
2024-07-10 19:29:14 +0000

    test: try to configure caddy

--- videostore/settings.py
+++ videostore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY") or "98e383483493047oasdj8230923jfjc90h
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["49.13.236.3", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*", "49.13.236.3", "127.0.0.1", "localhost"]
 CORS_ALLOWED_ORIGINS = ["https://videostore-frontend.vercel.app", "https://videostore.daniel-rubin.de",
                         "http://localhost:3000"]
 
@@ -198,6 +198,13 @@ CELERY_RESULT_BACKEND = "django-db"
 CELERY_CACHE_BACKEND = "django-cache"
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
